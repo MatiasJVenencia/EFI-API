dbfbd5041bb95fe676a7b3c785631a3d28c5f610
Ramiro Quiroga <r.quiroga@itecriocuarto.org.ar>
2024-10-08 18:35:00 -0300

    crear migraciones

---
+++ src/migrations/20241008211417-create-repair-order.js
@@ -0,0 +1,54 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('RepairOrders', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      fecha: {
+        type: Sequelize.DATE
+      },
+      problema_reportado: {
+        type: Sequelize.STRING
+      },
+      id_dispositivo: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Devices', 
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+      },
+      id_usuario: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users', 
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+      },
+      costo_estimado: {
+        type: Sequelize.DECIMAL
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('RepairOrders');
+  }
+};
