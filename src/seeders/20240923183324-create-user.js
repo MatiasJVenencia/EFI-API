56fdfb61c455724d2f56cc474a17ec6abe1b8e39
Ramiro Quiroga <r.quiroga@itecriocuarto.org.ar>
2024-11-04 22:49:19 -0300

    cofigurar seeder users

--- src/seeders/20240923183324-create-user.js
+++ src/seeders/20240923183324-create-user.js
@@ -1,28 +1,40 @@
 'use strict';
 
+const bcrypt = require('bcryptjs');
+const saltRounds = 10;
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Users', [
+    const users = [
       {
-        name: 'Juan Perez',
-        email: 'juan@gmail.com',
+        name: 'admin',
+        email: 'admin@admin.com',
         password: 'password123',
         role: 'admin',
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
-        name: 'Leandro Brey',
-        email: 'brey@gmail.com',
+        name: 'tecnico',
+        email: 'tecnico@tecnico.com',
         password: 'password123',
         role: 'tecnico',
         createdAt: new Date(),
         updatedAt: new Date(),
-      }
-    ], {});
+      },
+    ];
+
+    const hashedUsers = await Promise.all(
+      users.map(async (user) => {
+        const hashedPassword = await bcrypt.hash(user.password, saltRounds);
+        return { ...user, password: hashedPassword };
+      })
+    );
+
+    await queryInterface.bulkInsert('Users', hashedUsers, {});
   },
 
   down: async (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('Users', null,{});
-  }
+    await queryInterface.bulkDelete('Users', null, {});
+  },
 };
